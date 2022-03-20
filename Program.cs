257a49a5c5eddb09d43c1f0b8182c9dbb712b25b
Sevdiye <78251930+Sevdiye@users.noreply.github.com>
2022-03-20 14:22:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using CarProject.Presentation;//Добавяне
+using CarProject.Business;
+using CarProject.Data.Model;
+using System;
+
+namespace CarProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Display display = new Display();
+        }
+    }
+}
